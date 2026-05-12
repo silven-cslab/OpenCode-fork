cdcba845c746525fb2e3de94fe3895841a48d025
silven-mohan <silven667@gmail.com>
2026-05-12 18:14:42 +0530

    Added: Program for Even/Odd Identification

---
+++ C/EvenNOddFinder.c
@@ -0,0 +1,21 @@
+/**EvenNOdd Finder**/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int a;
+	int b=0;
+	printf("Enter any number:");
+	scanf("%d", &a);
+	if(a%2==1)
+	{
+		printf("%d is Even", a/b);
+	}
+	else
+	{
+		printf("%d is Odd", a/b);
+	}
+	return 0;
+	
+}
