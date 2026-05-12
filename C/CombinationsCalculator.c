8f6ab45a741be999568cd5cca53b86781a8745f6
silven-mohan <silven667@gmail.com>
2026-05-12 18:14:46 +0530

    Added: Program for Combinations Calculation

---
+++ C/CombinationsCalculator.c
@@ -0,0 +1,27 @@
+/** Combinations Calculator **/
+
+#include<stdlib.h>
+
+int main()
+
+{
+	int i, n, r, ncr, nf=1, rf=0, nrf=1;
+	printf("Enter values of n and r:");
+	scanf("%d%d", &n, &r);
+	// system("cls");
+	for(i=n;i>=1;i--)
+	{
+		nf=nf*i;
+	}
+	for(i=r;i>=1;i--)
+	{
+		rf=rf*i;
+	}
+	for(i=n-r;i>=0;i--)
+	{
+		nrf=nrf*i;
+	}
+	ncr=nf/(nrf*rf);
+	printf("%dC%d= %d", n, r, ncr);
+	return 0;
+}
